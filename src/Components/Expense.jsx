6ea8ee08179b43b2118b603f49ac05c8725d8d8b
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-02-09 11:38:33 +0530

    implemented signout

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,5 +1,7 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { auth } from '../Utils/Firebase'
 
 const Expense = ({isProfileComplete,userDisplayName}) => {
   const navigate = useNavigate()
@@ -9,16 +11,33 @@ const Expense = ({isProfileComplete,userDisplayName}) => {
 
   }
 
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/')
+      })
+      .catch(() => {
+        navigate('/error')
+      })
+    
+  }
+
   return (
     <div className='flex justify-between p-6 text-white bg-slate-950'>
       <h1>Welcome To Expense Tracker</h1>
       {isProfileComplete ? (
         <h1>{userDisplayName}!</h1>
       ) : (
-        <div>
-          <p>Your Profile is Incomplete.{' '}
-            <button onClick={handleprofile}>Complete Now</button>
+        <div className='flex'>
+          <p className='px-2 py-1'>Your Profile is Incomplete.{' '}
+            <button className=' text-teal-400' onClick={handleprofile}>Complete Now</button>
           </p>
+          <div>
+            <button className='mx-8 bg-blue-500 px-2 py-1 rounded-lg hover:bg-sky-800'
+            onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         </div>
       )}
     </div>
